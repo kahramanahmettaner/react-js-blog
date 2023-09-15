9fae718fcd5be0b20848579e7ff1e517c7d076b7
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-16 01:05:14 +0200

    Add components and routes
    added the components Header, Nav, Footer and the routes Home, PostPage, NewPost, About, Missing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './index.css';
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Home from './routes/Home.js'
+import NewPost from './routes/NewPost.js'
+import PostPage from './routes/PostPage.js'
+import About from './routes/About.js'
+import Missing from './routes/Missing.js'
+import { Route, Switch, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header/>
+      <Nav/>
+
+      <Switch>
+        <Route exact path="/"> 
+          <Home/>
+        </Route> 
+        <Route exact path="/post">
+          <NewPost/>
+        </Route>
+        <Route path="/post/:id">
+          <PostPage/>
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      
+      <Footer/>
     </div>
   );
 }
