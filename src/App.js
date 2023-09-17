a65fcea50c961bdf0402ab7a7f567a47d097b55d
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-17 23:32:56 +0200

    Add axios api requests and add new route EditPost.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Missing from './routes/Missing.js'
 import { Route, Switch, useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts'
+import EditPost from './EditPost';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -17,7 +19,32 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        /* if (response && response.data) */
+        setPosts(response.data); // axios will automatically catch anything outside the 200-range 
+      } catch (err) {
+        // from axios documentation
+        if (err.response) {
+          // Not in the 200 response range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+
+      }
+    }
+
+    fetchPosts();
+  }, [])
 
   useEffect( () => {
     const filteredResults = posts.filter( post => 
@@ -28,22 +55,47 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [ ...posts, newPost ];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    history.push('/')
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [ ...posts, response.data ];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      history.push('/')
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }    
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    history.push('/');
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      // could use "patch" to update specific fields but here "put" to update the entire post
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map( post => post.id === id ? { ...response.data } : post ));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    } 
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`)
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      history.push('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }    
   }
 
   return (
@@ -62,6 +114,16 @@ function App() {
             setPostTitle={setPostTitle}
             postBody={postBody}
             setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/edit/:id">
+          <EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
           />
         </Route>
         <Route path="/post/:id">
--- src/routes/PostPage.js
+++ src/routes/PostPage.js
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom/cjs/react-router-dom.min"
+import { useParams, Link } from "react-router-dom"
 
 const PostPage = ({ posts, handleDelete }) => {
   const { id } = useParams();
@@ -11,7 +11,10 @@ const PostPage = ({ posts, handleDelete }) => {
               <h2>{post.title}</h2>
               <p className='postDate'>{post.datetime}</p>
               <p className='postBody'>{post.body}</p>
-              <button onClick={() => handleDelete(post.d)}>
+              <Link to={`/edit/${post.id}`}>
+                  <button className="editButton">Edit Post</button>
+              </Link>
+              <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                 Delete Post
               </button>
             </>
