a5892b31d3d59342df265c1b4a5399654c1f36eb
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-17 22:04:00 +0200

    Implement the components and the routes

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+export const Post = ({ post }) => { 
+  return (
+    <article className='post'>
+        <Link to={`/post/${post.id}`}>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+        </Link>
+        <p className='postBody'> {
+            (post.body).length <= 25
+                ? post.body
+                : `${(post.body).slice(0, 25)}...`
+        }
+        </p>
+    </article>
+  )
+}
