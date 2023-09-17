f3a65fc585100b29421d12f6ed2606555fca3071
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-17 22:14:17 +0200

    Fix typing error with css class

--- src/routes/NewPost.js
+++ src/routes/NewPost.js
@@ -2,7 +2,7 @@ const NewPost = ({
   handleSubmit, postTitle, setPostTitle, postBody, setPostBody 
 }) => {
     return (
-      <main className="newPost">
+      <main className="NewPost">
           <h2>New Post</h2>
           <form className="newPostForm" onSubmit={handleSubmit}>
             <label htmlFor="postTitle">Title:</label>
